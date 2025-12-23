d4c5160fd94abd499d4f91ce1cc0a0548b98845d
Roman Chursanov <roman@chrsnv.ru>
2025-12-23 10:57:11 +0500

    feat(models): Экспорт новых моделей

--- stp_database/models/Stats/__init__.py
+++ stp_database/models/Stats/__init__.py
@@ -1,7 +1,19 @@
 """Инициализация моделей Stats."""
 
 from .head_premium import HeadPremium
+from .sl import SL
 from .spec_kpi import SpecDayKPI, SpecMonthKPI, SpecWeekKPI
 from .spec_premium import SpecPremium
+from .tests import AssignedTest
+from .tutors_schedule import TutorsSchedule
 
-__all__ = ["HeadPremium", "SpecDayKPI", "SpecMonthKPI", "SpecWeekKPI", "SpecPremium"]
+__all__ = [
+    "HeadPremium",
+    "SpecDayKPI",
+    "SpecMonthKPI",
+    "SpecWeekKPI",
+    "SpecPremium",
+    "SL",
+    "AssignedTest",
+    "TutorsSchedule",
+]
